7aef4aebd7b19df2c4ecc13340534695fd5b870d
Cuci07 <161069495+Cuci07@users.noreply.github.com>
2025-02-14 17:01:00 +0100

    Add files via upload

---
+++ blockchain_kodi_detyrakursi.py
@@ -0,0 +1,57 @@
+import hashlib
+import time
+import json
+
+class Block:
+    def __init__(self, index, previous_hash, timestamp, data, nonce=0):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.nonce = nonce
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = f"{self.index}{self.previous_hash}{self.timestamp}{self.data}{self.nonce}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+        self.difficulty = 4  # Adjust for faster or slower mining
+
+    def create_genesis_block(self):
+        return Block(0, "0", time.time(), "Genesis Block")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+    def proof_of_work(self, block):
+        while block.hash[:self.difficulty] != "0" * self.difficulty:
+            block.nonce += 1
+            block.hash = block.calculate_hash()
+        return block
+
+    def validate_chain(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+            if current_block.hash != current_block.calculate_hash():
+                return False
+            if current_block.previous_hash != previous_block.hash:
+                return False
+        return True
+
+# Example Usage
+if __name__ == "__main__":
+    blockchain = Blockchain()
+    block1 = Block(1, blockchain.get_latest_block().hash, time.time(), "Transaction Data")
+    blockchain.proof_of_work(block1)
+    blockchain.add_block(block1)
+
+    print("Blockchain is valid:", blockchain.validate_chain())
